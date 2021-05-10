76373cd4b75434fb6fbfb9bfe030bd3915a42536
Sahil Rajput <sahilrajput03@gmail.com>
2021-05-11 00:46:39 +0530

    Add more to readme🧄︎. ~ sahil

---
+++ 5.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$txt = "Hello world!";
+$x = 5;
+$y = 10.5;
+
+echo $txt;
+echo "<br>";
+echo $x;
+echo "<br>";
+echo $y;
+?>
+
+<p>
+PHP Variables
+
+A variable can have a short name (like x and y) or a more descriptive name (age, carname, total_volume).
+
+Rules for PHP variables:
+
+    A variable starts with the $ sign, followed by the name of the variable
+    A variable name must start with a letter or the underscore character
+    A variable name cannot start with a number
+    A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+    Variable names are case-sensitive ($age and $AGE are two different variables)
+    <br/>
+    <a href="https://www.w3schools.com/php/php_variables.asp">source</a>
+    
+</p>
+
+
+</body>
+</html>
--- Readme.md
+++ Readme.md
@@ -7,3 +7,7 @@ To serve a particular path with apache2, read the guide @ linux-walkthrough.md f
 - The file name must have extension .php otherwise the html content will render for sure but the php code won't execute at all.
 
 Browse `localhost/docs` to view the default page of apache2 server.
+
+## continue doing from below link
+
+https://www.w3schools.com/php/php_variables.asp
